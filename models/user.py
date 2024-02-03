e2adbd5444bfd819828fbe8b66e179e41e6fdb83
Meriam-elyns <meriamelyns@gmail.com>
2024-02-03 09:20:01 -0800

    q

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -19,11 +20,23 @@ class User(BaseModel, Base):
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
-        email = " gui@hbtn.io "
-        password = " guipwd "
-        first_name = " Guillaume "
-        last_name = " Snow "
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """
+            instantiates user object
+        """
+        if kwargs:
+            pwd = kwargs.pop('password', None)
+            if pwd:
+                # Hash the password using MD5
+                secure = hashlib.md5()
+                secure.update(pwd.encode("utf-8"))
+                secure_password = secure.hexdigest()
+                kwargs['password'] = secure_password
         super().__init__(*args, **kwargs)
+
+
